14e60cdeb6f442f9bce12de2acf0ffe727fd1145
Rishi Kumar <kuldeeprishisharma098@gmail.com>
2026-09-19 01:04:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Min and Max in Array/min-and-max-in-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ArrayList<Integer> getMinMax(int[] arr) {
+        
+        // code Here
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+       ArrayList<Integer> ans=new ArrayList<>();
+       ans.add(min);
+       ans.add(max);
+       return ans;
+    }
+}
